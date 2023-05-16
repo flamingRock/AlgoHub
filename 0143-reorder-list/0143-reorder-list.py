9bfe8d961ab6ac07ef34bebcaab6326611b9e553
flamingRock <bbaecheol@gmail.com>
2023-05-17 01:08:58 +0900

    Time: 106 ms (18.88%), Space: 25.9 MB (28.52%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # LL를 array로 만들면서 length 재기
+        arr = []
+        cur, length = head, 0
+        
+        while cur:
+            arr.append(cur)
+            cur, length = cur.next, length + 1
+        
+        # 두 pointer(left, right)로 좌우 번갈아가면서 LL에 넣기
+        # 마지막 노드는 last로
+        left, right = 0, length - 1
+        last = head
+        
+        while left < right:
+            arr[left].next = arr[right]
+            left += 1
+            
+            if left == right:
+                last = arr[right]
+                break
+            
+            arr[right].next = arr[left]
+            right -= 1
+            
+            last = arr[left]
+        
+        if last:
+            last.next = None
