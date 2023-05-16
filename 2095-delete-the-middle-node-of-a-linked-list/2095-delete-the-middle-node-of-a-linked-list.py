851bc0d0c2d5c67019df31a2704728e0fa6f2bce
flamingRock <bbaecheol@gmail.com>
2023-05-16 09:56:54 +0900

    Time: 1722 ms (78.48%), Space: 63.4 MB (10.33%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -6,33 +6,50 @@
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
 #       # 중간 값은 다음다음과 다음의 차이에 있다''를 사용하는 방식
-#         if not head.next:
-#             return head.next
+        # if not head.next:
+        #     return head.next
         
-#         fast, slow = head.next.next, head
+        # fast, slow = head.next.next, head
         
-#         while fast and fast.next:
-#             slow = slow.next
-#             fast = fast.next.next
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
         
-#         slow.next = slow.next.next
-#         return head
+        # slow.next = slow.next.next
+        # return head
+
+        # 위의 방식에서 try-except로 실행속도 개선
+        fast, slow, slow_before = head, head, head
+        while fast != None:
+            try:
+                fast = fast.next.next
+                slow_before = slow
+                slow = slow.next
+            except:
+                break
         
-        
-      # 단순히 중간 index를 구해 해당 Node를 delete하는 방식
-        if not head or not head.next:
+        try:
+            slow_before.next = slow_before.next.next
+        except:
             return None
         
-        length = 1
-        is_null = head
-        while is_null.next:
-            length += 1
-            is_null = is_null.next
-            
-        mid_index = length // 2
-        pre_mid_node = head
-        for i in range(mid_index - 1):
-            pre_mid_node = pre_mid_node.next
-        pre_mid_node.next = pre_mid_node.next.next
-        
         return head
+        
+        
+#       # 단순히 중간 index를 구해 해당 Node를 delete하는 방식
+#         if not head or not head.next:
+#             return None
+        
+#         length = 1
+#         is_null = head
+#         while is_null.next:
+#             length += 1
+#             is_null = is_null.next
+            
+#         mid_index = length // 2
+#         pre_mid_node = head
+#         for i in range(mid_index - 1):
+#             pre_mid_node = pre_mid_node.next
+#         pre_mid_node.next = pre_mid_node.next.next
+        
+#         return head
